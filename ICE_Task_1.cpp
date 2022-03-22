cfc4ef6cb7ed53c4945bf49bead40d757da0c549
ppatoria <ppatoria@gmail.com>
2022-03-21 21:28:19 -0400

    test

--- ICE_Task_1.cpp
+++ ICE_Task_1.cpp
@@ -1,81 +1,111 @@
-﻿// CPP program to find rank of an
-// element in a stream.
-//#include <bits/stdc++.h>
+﻿/**
+*   Interview Question:
+*   Write a program that does the following:
+*   Read a double precision number (from stdin) and print the smallest number that is larger than 95% of numbers encountered so far.
+*   Run program on input size = 128,000,000 and provide the time taken. Provide the time taken for this random input along with the solution.
+**/
 #include<iostream>
 #include <memory>
+#include <random>
 
 using namespace std;
 
 struct node
 {
-	int data;
-	node* left, * right;
-	int left_size;
+	double	data;
+	node	*left, * right;
+	int		left_size;
 
-	node(int data) : data(data), left(nullptr), right(nullptr), left_size(0)
+	node(double data) : 
+		data(data), 
+		left(nullptr), 
+		right(nullptr), 
+		left_size(0)
 	{}
-
 };
 
-node* insert(node*& root, int data)
+
+class bst
 {
-	if (!root)
-		return new node(data);
+	float _total_nodes;
+	node* _root;
 
-	if (data <= root->data)
-	{
-		root->left = insert(root->left, data);
-		root->left_size++;
-	}
-	else
-	{
-		root->right = insert(root->right, data);
-	}
-	return root;
-}
+public:
 
-int getRank(node* root, int x)
-{
-	if (root->data == x)
-		return root->left_size;
+	bst() :_total_nodes(0), _root(nullptr)
+	{}
 
-	if (x < root->data)
+	bool insert_while(int larger_than_percent, double data)
 	{
-		return (root->left == nullptr)
-			? -1
-			: getRank(root->left, x);
+		_total_nodes = _total_nodes + 1;
+		_root = insert(_root, data);
+		return (getPercent(data) >= 95);
 	}
-	else
+
+	node* insert(node*& root, double data)
 	{
-		if (!root->right)
-			return -1;
-		else {
-			int right_size = getRank(root->right, x);
-			if (right_size == -1) return -1;
-			return root->left_size + 1 + right_size;
+		if (!root)
+			return new node(data);
+
+		if (data <= root->data)
+		{
+			root->left = insert(root->left, data);
+			root->left_size++;
+		}
+		else
+		{
+			root->right = insert(root->right, data);
+		}		
+		return root;
+	}
+
+	int getRank(double x)
+	{
+		return getRank(_root, x);
+	}
+	
+	double getPercent(double x)
+	{
+		auto rank = getRank(x);
+		double fraction = (rank / _total_nodes);
+		double percent =  fraction * 100;
+		return percent;
+	}
+
+	int getRank(node* root, double x)
+	{
+		if (root->data == x)
+			return root->left_size;
+
+		if (x < root->data)
+		{
+			return (root->left == nullptr)
+				? -1
+				: getRank(root->left, x);
+		}
+		else
+		{
+			if (!root->right)
+				return -1;
+			else {
+				int right_size = getRank(root->right, x);
+				if (right_size == -1) return -1;
+				return root->left_size + 1 + right_size;
+			}
 		}
 	}
-}
+};
 
 int main()
 {
-	int arr[] = { 5, 1, 4, 4, 5, 9, 7, 13, 3 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int x = 4;
-
-	node* root = NULL;
-	for (int i = 0; i < n; i++)
-		root = insert(root, arr[i]);
-
-	std::cout << "Rank of " << x << " in stream is: "
-		<< getRank(root, x) << std::endl;
-
-	x = 13;
-	cout << "Rank of " << x << " in stream is: "
-		<< getRank(root, x) << endl;
-
-	x = 8;
-	cout << "Rank of " << x << " in stream is: "
-		<< getRank(root, x) << endl;
+	constexpr int MIN = 10;
+	constexpr int MAX = 100;
+	bst tree;
+	std::random_device rd;
+	std::default_random_engine eng(rd());
+	std::uniform_real_distribution<double> distr(MIN, MAX);
+	double input = distr(eng);
+	std::cout << input << std::endl;		
+	while(!tree.insert_while(95, input));
 	return 0;
 }
