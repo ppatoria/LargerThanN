de3cf68daa68126a83b1a8f9a449697453629630
ppatoria <ppatoria@gmail.com>
2022-03-21 23:15:50 -0400

    Final

--- larger_than_n.cpp
+++ larger_than_n.cpp
@@ -1,8 +1,5 @@
-﻿/**
-*   Interview Question:
-*   Write a program that does the following:
-*   Read a double precision number (from stdin) and print the smallest number that is larger than 95% of numbers encountered so far.
-*   Run program on input size = 128,000,000 and provide the time taken. Provide the time taken for this random input along with the solution.
+﻿/** 
+* Print the smallest number that is larger than 95 % of numbers encountered so far.
 **/
 #include<iostream>
 #include <memory>
